d17c3f1eec8121d4ff5b83a9caf8ddbfbf7d9145
marcopriests <prtmrc98@gmail.com>
2025-06-16 17:50:54 +0200

    [UPD] added card-link class to latest products cards

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -81,7 +81,7 @@ const Homepage = () => {
                             {/* cards */}
                             {latestProducts.map((product) => (
                                 <div className="col-4 col-md-3" key={product.id}>
-                                    <Link
+                                    <Link className='card-link'
                                         to={`/product/${product.id}`}>
                                         <Cards product={product} />
                                     </Link>
