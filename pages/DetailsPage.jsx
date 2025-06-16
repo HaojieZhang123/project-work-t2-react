539595ade86d8f9c6029f74ba7ea1778fd59c1af
HaojieZhang123 <haojiezhang123@gmail.com>
2025-06-16 11:34:04 +0200

    [UPD] axios call to fetch product in homepage and in details page

--- pages/DetailsPage.jsx
+++ pages/DetailsPage.jsx
@@ -1,10 +1,31 @@
 import { useParams } from 'react-router-dom';
-import products from '../data/products';
+// import products from '../data/products';
 import { Link } from 'react-router-dom';
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const DetailsPage = () => {
     const { id } = useParams();
-    const product = products.find(p => p.id === id);
+
+    const [product, setProduct] = useState([]);
+    const endpoint = `http://localhost:3000/api/products/${id}`;
+
+    // function to fetch product
+    const fetchProduct = () => {
+        axios.get(endpoint)
+            .then(response => {
+                setProduct(response.data);
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error("There was an error fetching the product!", error);
+            });
+    };
+
+    useEffect(() => {
+        fetchProduct();
+    }, []);
 
     if (!product) {
         return <div>Product not found</div>;
@@ -15,7 +36,7 @@ const DetailsPage = () => {
             <h2>{product.product_name}</h2>
             <img src={product.image} alt={product.product_name} />
             <p >{product.description}</p>
-            <h3>Pice: €{product.price}</h3>
+            <h3>Price: €{product.price}</h3>
             <p><strong>Category:</strong> {product.category}</p>
             <p><strong>Brand:</strong> {product.brand_id}</p>
             <Link to="/">
--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import Cards from '../components/Cards'
 import { Link } from 'react-router-dom'
-import products from '../data/products'
+// import products from '../data/products'
 
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Homepage = () => {
+
+    const [products, setProducts] = useState([])
+    const endpoint = 'http://localhost:3000/api/products/'
+
+    // function for fetch products via axios
+    const fetchProducts = () => {
+        axios.get(endpoint)
+            .then(response => {
+                setProducts(response.data)
+                console.log(products)
+                console.log(response.data)
+            })
+            .catch(error => {
+                console.error("There was an error fetching the products!", error);
+            });
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
     return (
         <>
             <div className="container">
