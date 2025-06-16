1f28d310be3998b956b50b8c4cd7aa581194aeba
marcopriests <prtmrc98@gmail.com>
2025-06-16 17:45:34 +0200

    [UPD] removed console.log

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Cards = ({ product }) => {
 
     const { image, product_name, brand_id, discount, price, added_date, sold } = product
-    console.log(product)
 
     const discountValue = (price * discount) / 100
 
@@ -30,12 +29,5 @@ const Cards = ({ product }) => {
         </div>
     )
 }
-{/* <img src={product.image} className="card-img-top" alt={product.product_name} />
-<div className="card-body">
-    <h5 className="card-title">{product.product_name}</h5>
-    <p className="card-text">{product.description}</p>
-    <p className="card-text"><strong>Prezzo:</strong> €{product.price}</p>
-    <p className="card-text"><small className="text-muted">{product.category}</small></p>
-</div> */}
 
 export default Cards
