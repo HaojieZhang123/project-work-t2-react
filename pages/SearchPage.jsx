85c6fd325bb2ef2283a59c7f26e7282fdb7c33aa
HaojieZhang123 <haojiezhang123@gmail.com>
2025-06-16 11:10:42 +0200

    [UPD] search page title placeholder

--- pages/SearchPage.jsx
+++ pages/SearchPage.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const SearchPage = () => {
     return (
         <>
-            Search page
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h2 className="my-5">642 RESULTS FOR SKIN CARE</h2>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
