e1074df39b4bcf7e853fb9c68294255d2429da4c
ErikaArci99 <erikaarci1999@outlook.it>
2025-06-17 12:44:46 +0200

    [UPD] added responsive to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,28 +2,26 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <>
-            <div className="container">
-                <div className="row">
-                    <div className="col-8">
-                        <ul className='d-flex justify-content-between list-unstyled'>
-                            <li>Privacy</li>
-                            <li>Contacts</li>
-                            <li>Recess rights</li>
-                            <li>Sell condition</li>
-                            <li>Compliance & Whistkeblowing</li>
-                            <li>Cookie policy</li>
-                        </ul>
-                    </div>
+        <div className="container py-3">
+            <div className="row">
+                <div className="col-12 col-md-8 mb-3 mb-md-0">
+                    <ul className='footer-list d-flex flex-wrap'>
+                        <li>Privacy</li>
+                        <li>Contacts</li>
+                        <li>Recess rights</li>
+                        <li>Sell condition</li>
+                        <li>Compliance & Whistleblowing</li>
+                        <li>Cookie policy</li>
+                    </ul>
+                </div>
 
-                    <div className="col-4">
-                        <div className="text-end">
-                            &copy; 2025 Boolean Italia S.p.A.
-                        </div>
+                <div className="col-12 col-md-4 d-flex justify-content-center align-items-center">
+                    <div>
+                        &copy; 2025 Boolean Italia S.p.A.
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
