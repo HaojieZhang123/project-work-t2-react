fb4829a07d47cb92a7cb91a8a8802e630f928d37
marcopriests <prtmrc98@gmail.com>
2025-06-17 11:03:54 +0200

    [UPD] alligned cards prices and tags

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -14,28 +14,31 @@ const Cards = ({ product }) => {
     const addedDate = new Date(added_date)
     const diffTime = today - addedDate
     const diffDays = diffTime / (1000 * 60 * 60 * 24)
-    const isNew = diffDays <= 15 && diffDays >= 0
+    const isNew = diffDays <= 28 && diffDays >= 0
 
     // Calcolo se il prodotto è "promo"
     const isPromo = discount > 0
 
     return (
         <div className="product-card color-main">
-            <div className="card-image-container">
-                <img src={image} alt="" />
-            </div>
             <div className="card-body-container">
+                <div className="card-image-container">
+                    <img src={image} alt="" />
+                </div>
                 <div className="card-brand color-main-subtle">Brand N°{brand_id}</div>
                 <div className="card-product-name">{product_name}</div>
                 <div className="card-category color-main-subtle">Categoria prodotto</div>
+            </div>
+            <div className="card-bottom-row">
                 <div className="card-price">
                     {`€ ${actualPrice.toFixed(2)}`}
                     {discount != 0 && <i className='card-original-price ms-2 color-main-subtle'>{`€ ${price}`}</i>}
                 </div>
-                {isPromo && <div className="card-tag">promo</div>}
-                {isNew && <div className="card-tag">new</div>}
+                <div>
+                    {isPromo && <div className="card-tag">promo</div>}
+                    {isNew && <div className="card-tag">new</div>}
+                </div>
             </div>
-
         </div>
     )
 }
