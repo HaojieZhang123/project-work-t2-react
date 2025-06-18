3065c2adc652edd85a5f982e2cf71fc9d411e746
ErikaArci99 <erikaarci1999@outlook.it>
2025-06-18 17:30:23 +0200

    [UPD] implemented WishList component layout with title and empty state message

--- pages/WishList.jsx
+++ pages/WishList.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const WishList = () => {
     return (
-        <div>WishList</div>
+        <div className='container pt-3 pb-3'>
+            <div className="row">
+                <div className="col-12">
+                    <h1 className='text-center'><i class="fa-regular fa-heart"></i>Wish List<i class="fa-regular fa-heart"></i></h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 d-flex justify-content-center">
+                    <p className='text-secondary'>Your wish list is empty.</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
