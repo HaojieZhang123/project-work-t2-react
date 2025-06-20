0531a1f1caab889d66c44aae03baa4a0753d6348
HaojieZhang123 <haojiezhang123@gmail.com>
2025-06-20 16:25:45 +0200

    [FEAT] mohbile cart overlay done

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,14 +1,39 @@
-import { Outlet } from "react-router-dom"
-
+// libraries
+import { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom"
+// components
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import SidebarCart from "../components/SidebarCart"
-
+import MobileCartOverlay from "../components/MobileCartOverlay"
+// contexts
 import { useCart } from '../context/CartContext';
 
 const DefaultLayout = () => {
+    // access to cart context
     const { cart } = useCart();
-    const isSidebarVisible = cart.length > 0;
+
+    // useLocation to determine the current path, hide sidebar and overlay when in cart page
+    const location = useLocation();
+    const isSidebarVisible = cart.length > 0 && location.pathname !== '/cart';
+
+    // state to manage the mobile cart overlay
+    const [showMobileCartOverlay, setShowMobileCartOverlay] = useState(false);
+    const [prevCartLength, setPrevCartLength] = useState(cart.length);
+
+    // Show overlay only when a new item is added (on mobile)
+    useEffect(() => {
+        if (
+            window.innerWidth < 768 &&
+            cart.length > prevCartLength &&
+            location.pathname !== '/cart'
+        ) {
+            setShowMobileCartOverlay(true);
+        }
+        setPrevCartLength(cart.length);
+        // eslint-disable-next-line
+    }, [cart.length]);
+
     return (
         <>
             <div className="d-flex flex-column min-vh-100">
@@ -27,9 +52,13 @@ const DefaultLayout = () => {
                 </div>
 
                 <div>
-                    <SidebarCart />
+                    {isSidebarVisible && <SidebarCart />}
                 </div>
             </div >
+
+            {showMobileCartOverlay && location.pathname !== '/cart' && (
+                <MobileCartOverlay onClose={() => setShowMobileCartOverlay(false)} />
+            )}
         </>
 
     )
